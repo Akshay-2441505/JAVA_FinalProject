0f8909bc2156fae8e6affa4e81fd07a62724e7d9
Akshay-2441505 <akshay.kurdekar@bcah.christuniversity.in>
2026-03-10 22:33:28 +0530

    Fix syntax and unresolved import in Borrower/Admin Apps

--- lending-client-admin/src/main/java/com/msme/admin/AdminApplication.java
+++ lending-client-admin/src/main/java/com/msme/admin/AdminApplication.java
@@ -3,7 +3,6 @@ package com.msme.admin;
 import atlantafx.base.theme.PrimerDark;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
--- lending-client-borrower/src/main/java/com/msme/borrower/BorrowerApplication.java
+++ lending-client-borrower/src/main/java/com/msme/borrower/BorrowerApplication.java
@@ -3,19 +3,18 @@ package com.msme.borrower;
 import atlantafx.base.theme.PrimerLight;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
 public class BorrowerApplication extends Application {
 
   @Override
-  public void start(Stage primaryStage) {
+  public void start(Stage primaryStage) throws Exception {
     // Apply AtlantaFX Primer Light theme for the borrower app
     Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
 
-    StackPane root = new StackPane(new Label("MSME Lending Borrower Application"));
-    Scene scene = new Scene(root, 800, 600);
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
+    Scene scene = new Scene(loader.load(), 1000, 700);
 
     primaryStage.setTitle("MSME Lending Sandbox - Borrower");
     primaryStage.setScene(scene);
